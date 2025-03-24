0dd10c6f75174fc62bb0c9e94d29282af5126c46
MarcinMq <marmarcin02@icloud.com>
2025-03-24 18:45:41 +0100

    Testy

---
+++ BESTTECH/src/main/java/com/aplikacjazespolowa/BESTTECH/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.aplikacjazespolowa.BESTTECH.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String testView() {
+        return "test";
+    }
+}
