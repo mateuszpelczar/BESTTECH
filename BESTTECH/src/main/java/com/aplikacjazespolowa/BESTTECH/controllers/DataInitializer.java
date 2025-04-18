20d626e4bfc173a0c6162e527d888f1fe4faaa29
Mateusz Pelczar <matrix1555555@gmail.com>
2025-04-18 10:11:18 +0200

    Dodano style strony glownej,logowania,rejestacji,logow oraz zmienio zabezpieczenia

--- BESTTECH/src/main/java/com/aplikacjazespolowa/BESTTECH/controllers/DataInitializer.java
+++ BESTTECH/src/main/java/com/aplikacjazespolowa/BESTTECH/controllers/DataInitializer.java
@@ -1,38 +1,38 @@
-package com.aplikacjazespolowa.BESTTECH.controllers;
-
-import com.aplikacjazespolowa.BESTTECH.models.DBRole;
-import com.aplikacjazespolowa.BESTTECH.models.DBRoleRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class DataInitializer implements CommandLineRunner {
-
-    private final DBRoleRepository roleRepository;
-
-    public DataInitializer(DBRoleRepository roleRepository) {
-        this.roleRepository = roleRepository;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        if (roleRepository.findByName("ADMIN").isEmpty()) {
-            DBRole adminRole = new DBRole();
-            adminRole.setName("ADMIN");
-            roleRepository.save(adminRole);
-        }
-
-        if (roleRepository.findByName("EMPLOYEE").isEmpty()) {
-            DBRole employeeRole = new DBRole();
-            employeeRole.setName("EMPLOYEE");
-            roleRepository.save(employeeRole);
-        }
-
-        if (roleRepository.findByName("CLIENT").isEmpty()) {
-            DBRole clientRole = new DBRole();
-            clientRole.setName("CLIENT");
-            roleRepository.save(clientRole);
-        }
-    }
-}
+//package com.aplikacjazespolowa.BESTTECH.controllers;
+//
+//import com.aplikacjazespolowa.BESTTECH.models.DBRole;
+//import com.aplikacjazespolowa.BESTTECH.models.DBRoleRepository;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class DataInitializer implements CommandLineRunner {
+//
+//    private final DBRoleRepository roleRepository;
+//
+//    public DataInitializer(DBRoleRepository roleRepository) {
+//        this.roleRepository = roleRepository;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//        if (roleRepository.findByName("ADMIN").isEmpty()) {
+//            DBRole adminRole = new DBRole();
+//            adminRole.setName("ADMIN");
+//            roleRepository.save(adminRole);
+//        }
+//
+//        if (roleRepository.findByName("EMPLOYEE").isEmpty()) {
+//            DBRole employeeRole = new DBRole();
+//            employeeRole.setName("EMPLOYEE");
+//            roleRepository.save(employeeRole);
+//        }
+//
+//        if (roleRepository.findByName("CLIENT").isEmpty()) {
+//            DBRole clientRole = new DBRole();
+//            clientRole.setName("CLIENT");
+//            roleRepository.save(clientRole);
+//        }
+//    }
+//}
--- BESTTECH/src/main/java/com/aplikacjazespolowa/BESTTECH/security/SecurityConfig.java
+++ BESTTECH/src/main/java/com/aplikacjazespolowa/BESTTECH/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.aplikacjazespolowa.BESTTECH.models.DBUser;
 import com.aplikacjazespolowa.BESTTECH.models.DBUserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,10 +26,26 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
 
-
+                .csrf(csrf -> csrf.disable()) // WYŁĄCZENIE CSRF
                 .authorizeHttpRequests(authorize -> authorize
+
+                        //publiczne endpointy
                         .requestMatchers("/","/konto/rejestracja", "/konto/logowanie", "/resources/**", "/css/**", "/js/**").permitAll()
-//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/products/showproducts","/kategoria/**").permitAll()
+
+                        //dostep dla admina i pracownika
+                        .requestMatchers(HttpMethod.GET, "/products/addproduct", "/products/addcategory","/products/editcategory/{id}","/employee/inventory","/products/showcategories").hasAnyRole("ADMIN", "EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/products/addproduct", "/products/addcategory","/products/editcategory","/products/deletecategory").hasAnyRole("ADMIN", "EMPLOYEE")
+
+                        //dostep tylko dla admina
+                        .requestMatchers(HttpMethod.GET,"/admin/manageusers","/admin/manageusers/changerole","/admin/manageemployess","/admin/logs","/admin/deleteemployee").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/admin/manageusers/changerole/save","/admin/deleteemployee","/admin/deleteemployee").hasRole("ADMIN")
+
+                        //dostep do koszyka dla zalogowanych users
+                        .requestMatchers(HttpMethod.GET,"/koszyk").authenticated()
+                        .requestMatchers(HttpMethod.POST,"/koszyk/**").authenticated()
+
+                        //domyslnie reszta wymaga logowania
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
