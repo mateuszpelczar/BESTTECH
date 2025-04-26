1155d592a2054adefa8d1c0016cd8466c69973ec
MarcinMq <marmarcin02@icloud.com>
2025-04-26 16:11:25 +0200

    System recenzji z moderacja opini działa

--- BESTTECH/src/main/java/com/aplikacjazespolowa/BESTTECH/models/RecenzjaRepository.java
+++ BESTTECH/src/main/java/com/aplikacjazespolowa/BESTTECH/models/RecenzjaRepository.java
@@ -2,6 +2,9 @@ package com.aplikacjazespolowa.BESTTECH.models;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecenzjaRepository extends JpaRepository<Recenzja, Integer> {
+    List<Recenzja> findByKlient(DBUser klient);
 }
